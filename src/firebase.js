f5d14d98c95e975b436179487761a6e0d747eae0
Kaleab Asfaw <50099796+Kaleab-A@users.noreply.github.com>
2023-11-04 11:15:01 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const app = firebase.initializeApp({
-	apiKey: 'AIzaSyBq13dub61kAtO34n2m_KJ4GLwRX7DMbng',
+	apiKey: 'REPLACE',
 	authDomain: 'room-investigation.firebaseapp.com',
 	projectId: 'room-investigation',
 	storageBucket: 'room-investigation.appspot.com',
-	messagingSenderId: '932578494942',
-	appId: '1:932578494942:web:7f20685ef1a41d229fb54f',
+	messagingSenderId: 'REPLACE',
+	appId: 'REPLACE',
 });
 
 export const auth = app.auth();
